dd4b3bb606ad4409d000e49bb950014073c2a911
minug0226 <dhsfkdls04@naver.com>
2022-04-12 23:56:38 +0900

    연습

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender?: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Blcok {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 18, "female");
-
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-
-console.log(sayHi(person));
+const genesisBlock: Blcok = new Blcok(0, "20202020220202", "", "Hello");
 
 export {};
